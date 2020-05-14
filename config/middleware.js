7d893ef1c3793e77a97858c04a07d673d1b358eb
Josh Goebel <me@joshgoebel.com>
2020-05-14 13:18:39 -0400

    add staticify for asset tagging, disable compression on backend

--- config/middleware.js
+++ config/middleware.js
@@ -5,34 +5,47 @@ const cookieParser = require('cookie-parser');
 const expressStaticGzip = require("express-static-gzip");
 const addRequestId = require('express-request-id')({setHeader: false})
 const morgan = require('morgan')
+const path = require('path');
+const staticify = require('staticify')(path.join(__dirname, '../public'));
 
 morgan.token('id', (req) => req.id.split('-')[0])
 
 function buildStack(routes) {
   const middleware = express();
 
+  // unique request ids
   middleware.use(addRequestId)
 
-  // middleware
+  // asset tagging
+  middleware.use((_req,resp,next) => {
+    resp.locals.asset = staticify.getVersionedPath
+    next();
+  })
+
+  // static asset routing
   if (process.env.NODE_ENV === 'production') {
-    middleware.use(expressStaticGzip('public', {index: false, enableBrotli: true}));
+    // middleware.use(expressStaticGzip('public', {index: false, enableBrotli: true}));
   } else {
     middleware.use(express.static('public'));
+    // only in dev mode, in production nginx handles this concern
+    middleware.use(staticify.middleware);
   }
 
-  // logging
+  // Rails like logging
   middleware.use(morgan(
     "[:date[iso] #:id] Started :method :url for :remote-addr",
     {immediate: true}))
   middleware.use(morgan("[:date[iso] #:id] Completed :status :res[content-length] in :response-time ms"))
 
+  // param and cookie parsing
   middleware.use(bodyParser.urlencoded({extended: false}));
   middleware.use(cookieParser());
-  const shouldCompress = (req, res) => { return req.path.slice(0,3) === '/p/' }
-  middleware.use(compression({ filter: shouldCompress }))
 
+  // const shouldCompress = (req, res) => { return req.path.slice(0,3) === '/p/' }
+  // middleware.use(compression({ filter: shouldCompress }))
+
+  // inject our routing stack
   middleware.use(routes)
-
 
   // errors
   middleware.use(function (err, _req, res, next) {
